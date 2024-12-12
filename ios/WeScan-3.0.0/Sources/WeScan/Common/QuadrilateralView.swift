510bfefcc9c2f84dca6b20b266f037b0d81864d2
Maxim Tripilets <mtripilec@gmail.com>
2024-12-13 00:06:13 +0100

    fix: small fix

--- ios/WeScan-3.0.0/Sources/WeScan/Common/QuadrilateralView.swift
+++ ios/WeScan-3.0.0/Sources/WeScan/Common/QuadrilateralView.swift
@@ -46,7 +46,7 @@ final class QuadrilateralView: UIView {
     public var editable = false {
         didSet {
             cornerViews(hidden: !editable)
-            quadLayer.fillColor = editable ? UIColor.purple.withAlphaComponent(0.6).cgColor : UIColor.purple.withAlphaComponent(0.5).cgColor
+            quadLayer.fillColor = editable ? UIColor(white: 0.0, alpha: 0.6).cgColor : UIColor.purple.withAlphaComponent(0.5).cgColor
             guard let quad else {
                 return
             }
